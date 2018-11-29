ff3b44ce64f206acf3029fcb7a9bb08f605f2169
Phil Davis <phil@jankaritech.com>
2018-11-29 18:26:04 +0545

    adjust acceptance test time constant names

--- tests/acceptance/features/lib/DisabledUserPage.php
+++ tests/acceptance/features/lib/DisabledUserPage.php
@@ -45,7 +45,7 @@ class DisabledUserPage extends OwncloudPage {
 	 */
 	public function waitTillPageIsLoaded(
 		Session $session,
-		$timeout_msec = STANDARDUIWAITTIMEOUTMILLISEC
+		$timeout_msec = STANDARD_UI_WAIT_TIMEOUT_MILLISEC
 	) {
 		$currentTime = \microtime(true);
 		$end = $currentTime + ($timeout_msec / 1000);
@@ -53,7 +53,7 @@ class DisabledUserPage extends OwncloudPage {
 			if ($this->findAll("xpath", $this->userDisabledXpath)) {
 				break;
 			}
-			\usleep(STANDARDSLEEPTIMEMICROSEC);
+			\usleep(STANDARD_SLEEP_TIME_MICROSEC);
 			$currentTime = \microtime(true);
 		}
 
